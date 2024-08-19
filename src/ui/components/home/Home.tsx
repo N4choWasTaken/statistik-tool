6e25b758660b9f3af72493e000b9481ef545f494
Nico Fortino <88322851+NicoUnderTheStairs@users.noreply.github.com>
2024-08-19 14:04:35 +0200

    added favicon, title and emojis✨

--- src/ui/components/home/Home.tsx
+++ src/ui/components/home/Home.tsx
@@ -1,16 +1,16 @@
 import ItemList from "../itemlist/ItemList";
 
 const item = [
-  { key: 1, name: "New Game", href: "/wizard" },
-  { key: 2, name: "Überblick Saisons", href: "/overview-saison" },
-  { key: 3, name: "Überblick Players", href: "/overview-players" },
+  { key: 1, name: "✨ New Game", href: "/wizard" },
+  { key: 2, name: "🔥 Überblick Saisons", href: "/overview-saison" },
+  { key: 3, name: "⚡️ Einzelstatistik Players", href: "/overview-players" },
 ];
 
 const Home = () => {
   return (
     <>
       <div className="home section">
-        <h1>VBC Limmattal HU23</h1>
+        <h1>🏐 VBC Limmattal HU23</h1>
       </div>
       <ItemList items={item} />
     </>
