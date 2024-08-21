08871356554da66830d43ee2b874d90900b23379
Nico <nico.fortino@bluewin.ch>
2024-08-21 15:44:03 +0200

    players loaded into active game

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -18,6 +18,8 @@ export interface Player {
 }
 
 export interface ResolvedPlayer extends Player {
+  Number: ReactNode;
+  Name: ReactNode;
   playerDetails?: User; 
 }
 
--- src/ui/components/gametable/GameTable.tsx
+++ src/ui/components/gametable/GameTable.tsx
@@ -1,16 +1,11 @@
-import usePlayers from "../../../hooks/usePlayers";
+import { useGame } from "../../../hooks/useGame";
 
 const GameTable = () => {
-  const { players, loading, error } = usePlayers();
-  console.log(players);
-
-  if (loading)
-    return (
-      <div className="loader__wrapper">
-        <div className="loader">🏐</div>
-      </div>
-    );
-  if (error) return <div>Error: {error.message}</div>;
+  // get gameId from url parameter gameid
+  const queryParameters = new URLSearchParams(window.location.search);
+  const gameid = queryParameters.get("gameid") ?? "";
+  const game = useGame(gameid);
+  const players = game.playersData;
 
   return (
     <div className="section">
@@ -34,29 +29,39 @@ const GameTable = () => {
           </tr>
 
           {/* loop here to spit out data */}
-          <tr className="simpletable__row">
-            <td className="gametable__row__field--player simpletable__row__field">
-              <span>Thierry</span> <span>#12</span>
-            </td>
-            <td className="gametable__row__field simpletable__row__field">A</td>
-            <td className="gametable__row__field simpletable__row__field">B</td>
-            <td className="gametable__row__field simpletable__row__field">S</td>
-            <td className="gametable__row__field simpletable__row__field">R</td>
-            <td className="gametable__row__field simpletable__row__field">
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M4.36011 1V0.5H3.36011V1H4.36011ZM3.50655 17.3536C3.70182 17.5488 4.0184 17.5488 4.21366 17.3536L7.39564 14.1716C7.5909 13.9763 7.5909 13.6597 7.39564 13.4645C7.20038 13.2692 6.8838 13.2692 6.68853 13.4645L3.86011 16.2929L1.03168 13.4645C0.836418 13.2692 0.519836 13.2692 0.324574 13.4645C0.129311 13.6597 0.129311 13.9763 0.324574 14.1716L3.50655 17.3536ZM13.3979 17V17.5H14.3979V17H13.3979ZM14.2515 0.646447C14.0562 0.451184 13.7396 0.451184 13.5444 0.646447L10.3624 3.82843C10.1671 4.02369 10.1671 4.34027 10.3624 4.53553C10.5577 4.7308 10.8742 4.7308 11.0695 4.53553L13.8979 1.70711L16.7264 4.53553C16.9216 4.7308 17.2382 4.7308 17.4335 4.53553C17.6287 4.34027 17.6287 4.02369 17.4335 3.82843L14.2515 0.646447ZM3.36011 1V17H4.36011V1H3.36011ZM14.3979 17V1H13.3979V17H14.3979Z"
-                  fill="black"
-                />
-              </svg>
-            </td>
-          </tr>
+          {players.map((player) => (
+            <tr className="simpletable__row" id={player.id} key={player.id}>
+              <td className="gametable__row__field--player simpletable__row__field">
+                <span>{player.Name}</span> <span>#{player.Number}</span>
+              </td>
+              <td className="gametable__row__field simpletable__row__field">
+                A
+              </td>
+              <td className="gametable__row__field simpletable__row__field">
+                B
+              </td>
+              <td className="gametable__row__field simpletable__row__field">
+                S
+              </td>
+              <td className="gametable__row__field simpletable__row__field">
+                R
+              </td>
+              <td className="gametable__row__field simpletable__row__field">
+                <svg
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4.36011 1V0.5H3.36011V1H4.36011ZM3.50655 17.3536C3.70182 17.5488 4.0184 17.5488 4.21366 17.3536L7.39564 14.1716C7.5909 13.9763 7.5909 13.6597 7.39564 13.4645C7.20038 13.2692 6.8838 13.2692 6.68853 13.4645L3.86011 16.2929L1.03168 13.4645C0.836418 13.2692 0.519836 13.2692 0.324574 13.4645C0.129311 13.6597 0.129311 13.9763 0.324574 14.1716L3.50655 17.3536ZM13.3979 17V17.5H14.3979V17H13.3979ZM14.2515 0.646447C14.0562 0.451184 13.7396 0.451184 13.5444 0.646447L10.3624 3.82843C10.1671 4.02369 10.1671 4.34027 10.3624 4.53553C10.5577 4.7308 10.8742 4.7308 11.0695 4.53553L13.8979 1.70711L16.7264 4.53553C16.9216 4.7308 17.2382 4.7308 17.4335 4.53553C17.6287 4.34027 17.6287 4.02369 17.4335 3.82843L14.2515 0.646447ZM3.36011 1V17H4.36011V1H3.36011ZM14.3979 17V1H13.3979V17H14.3979Z"
+                    fill="black"
+                  />
+                </svg>
+              </td>
+            </tr>
+          ))}
           {/* loop here to spit out data */}
         </tbody>
       </table>
