7dc2e00ce58f0cb3690d4590259933a98824354a
Nico Fortino <88322851+NicoUnderTheStairs@users.noreply.github.com>
2024-09-02 13:37:55 +0200

    dont display playerstat in profile if playerRef is not set

--- src/ui/components/profile/Profile.tsx
+++ src/ui/components/profile/Profile.tsx
@@ -33,6 +33,14 @@ const Profile = () => {
   //@ts-ignore
   const userData = useGetUserData(currentUser?.uid ?? '');
 
+  if (!userData) {
+    return (
+      <div className="loader__wrapper">
+        <div className="loader">🏐</div>
+      </div>
+    );
+  }
+
   players.forEach((possiblePlayer) => {
     if (possiblePlayer.id === userData.playerRef) {
       //@ts-ignore
@@ -69,206 +77,217 @@ const Profile = () => {
             Role: {userData ? `${userData.role}` : null}
           </h5>
         </div>
+        {userData.playerRef ? (
+          <>
+            <div className="section">
+              <table className="simpletable tablehightlight">
+                <tbody>
+                  <tr className="simpletable__title">
+                    <th className="simpletable__title__field--playerdetail">
+                      Overall
+                    </th>
+                    <th className="simpletable__title__field--playerdetail"></th>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Games played</td>
+                    <td className="simpletable__row__field">
+                      {player.gamesPlayed}
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Points scored</td>
+                    <td className="simpletable__row__field">
+                      {player.attack.kill +
+                        player.block.kill +
+                        player.service.ace}
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Errors</td>
+                    <td className="simpletable__row__field">
+                      {player.attack.error +
+                        player.block.error +
+                        player.service.error +
+                        player.receive.error}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
 
-        <div className="section">
-          <table className="simpletable tablehightlight">
-            <tbody>
-              <tr className="simpletable__title">
-                <th className="simpletable__title__field--playerdetail">
-                  Overall
-                </th>
-                <th className="simpletable__title__field--playerdetail"></th>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Games played</td>
-                <td className="simpletable__row__field">
-                  {player.gamesPlayed}
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Points scored</td>
-                <td className="simpletable__row__field">
-                  {player.attack.kill + player.block.kill + player.service.ace}
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Errors</td>
-                <td className="simpletable__row__field">
-                  {player.attack.error +
-                    player.block.error +
-                    player.service.error +
-                    player.receive.error}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+            {/* Attack */}
+            <div className="section">
+              <table className="simpletable tablehightlight">
+                <tbody>
+                  <tr className="simpletable__title">
+                    <th className="simpletable__title__field--playerdetail">
+                      Attack
+                    </th>
+                    <th className="simpletable__title__field--playerdetail"></th>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Kill %</td>
+                    <td className="simpletable__row__field">
+                      {Math.max(
+                        (player.attack.kill / player.attack.hits) * 100 || 0,
+                        0
+                      ).toFixed(0)}
+                      %
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">
+                      Attack Efficency
+                    </td>
+                    <td className="simpletable__row__field">
+                      {Math.max(
+                        ((player.attack.kill - player.attack.error) /
+                          player.attack.hits) *
+                          100 || 0,
+                        0
+                      ).toFixed(0)}
+                      %
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Hits</td>
+                    <td className="simpletable__row__field">
+                      {player.attack.hits}
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Kills</td>
+                    <td className="simpletable__row__field">
+                      {player.attack.kill}
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Errors</td>
+                    <td className="simpletable__row__field">
+                      {player.attack.error}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
 
-        {/* Attack */}
-        <div className="section">
-          <table className="simpletable tablehightlight">
-            <tbody>
-              <tr className="simpletable__title">
-                <th className="simpletable__title__field--playerdetail">
-                  Attack
-                </th>
-                <th className="simpletable__title__field--playerdetail"></th>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Kill %</td>
-                <td className="simpletable__row__field">
-                  {Math.max(
-                    (player.attack.kill / player.attack.hits) * 100 || 0,
-                    0
-                  ).toFixed(0)}
-                  %
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Attack Efficency</td>
-                <td className="simpletable__row__field">
-                  {Math.max(
-                    ((player.attack.kill - player.attack.error) /
-                      player.attack.hits) *
-                      100 || 0,
-                    0
-                  ).toFixed(0)}
-                  %
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Hits</td>
-                <td className="simpletable__row__field">
-                  {player.attack.hits}
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Kills</td>
-                <td className="simpletable__row__field">
-                  {player.attack.kill}
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Errors</td>
-                <td className="simpletable__row__field">
-                  {player.attack.error}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+            {/* Block */}
+            <div className="section">
+              <table className="simpletable tablehightlight">
+                <tbody>
+                  <tr className="simpletable__title">
+                    <th className="simpletable__title__field--playerdetail">
+                      Block
+                    </th>
+                    <th className="simpletable__title__field--playerdetail"></th>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Kills</td>
+                    <td className="simpletable__row__field">
+                      {player.block.kill}
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Errors</td>
+                    <td className="simpletable__row__field">
+                      {player.block.error}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
 
-        {/* Block */}
-        <div className="section">
-          <table className="simpletable tablehightlight">
-            <tbody>
-              <tr className="simpletable__title">
-                <th className="simpletable__title__field--playerdetail">
-                  Block
-                </th>
-                <th className="simpletable__title__field--playerdetail"></th>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Kills</td>
-                <td className="simpletable__row__field">{player.block.kill}</td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Errors</td>
-                <td className="simpletable__row__field">
-                  {player.block.error}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+            {/* service */}
+            <div className="section">
+              <table className="simpletable tablehightlight">
+                <tbody>
+                  <tr className="simpletable__title">
+                    <th className="simpletable__title__field--playerdetail">
+                      Service
+                    </th>
+                    <th className="simpletable__title__field--playerdetail"></th>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Service %</td>
+                    <td className="simpletable__row__field">
+                      {Math.max(
+                        ((player.service.ace - player.service.error) /
+                          player.service.neutral) *
+                          100 || 0,
+                        0
+                      ).toFixed(0)}
+                      %
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">
+                      Service Efficency
+                    </td>
+                    <td className="simpletable__row__field">
+                      {Math.max(
+                        ((player.service.ace - player.service.error) /
+                          player.service.neutral) *
+                          100 || 0,
+                        0
+                      ).toFixed(0)}
+                      %
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Services</td>
+                    <td className="simpletable__row__field">
+                      {player.service.neutral}
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Ace</td>
+                    <td className="simpletable__row__field">
+                      {player.service.ace}
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Errors</td>
+                    <td className="simpletable__row__field">
+                      {player.service.error}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
 
-        {/* service */}
-        <div className="section">
-          <table className="simpletable tablehightlight">
-            <tbody>
-              <tr className="simpletable__title">
-                <th className="simpletable__title__field--playerdetail">
-                  Service
-                </th>
-                <th className="simpletable__title__field--playerdetail"></th>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Service %</td>
-                <td className="simpletable__row__field">
-                  {Math.max(
-                    ((player.service.ace - player.service.error) /
-                      player.service.neutral) *
-                      100 || 0,
-                    0
-                  ).toFixed(0)}
-                  %
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Service Efficency</td>
-                <td className="simpletable__row__field">
-                  {Math.max(
-                    ((player.service.ace - player.service.error) /
-                      player.service.neutral) *
-                      100 || 0,
-                    0
-                  ).toFixed(0)}
-                  %
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Services</td>
-                <td className="simpletable__row__field">
-                  {player.service.neutral}
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Ace</td>
-                <td className="simpletable__row__field">
-                  {player.service.ace}
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Errors</td>
-                <td className="simpletable__row__field">
-                  {player.service.error}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        {/* Receive */}
-        <div className="section">
-          <table className="simpletable tablehightlight">
-            <tbody>
-              <tr className="simpletable__title">
-                <th className="simpletable__title__field--playerdetail">
-                  Receive
-                </th>
-                <th className="simpletable__title__field--playerdetail"></th>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Positive</td>
-                <td className="simpletable__row__field">
-                  {player.receive.positive}
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Negative</td>
-                <td className="simpletable__row__field">
-                  {player.receive.negative}
-                </td>
-              </tr>
-              <tr className="simpletable__row">
-                <td className="simpletable__row__field">Errors</td>
-                <td className="simpletable__row__field">
-                  {player.receive.error}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+            {/* Receive */}
+            <div className="section">
+              <table className="simpletable tablehightlight">
+                <tbody>
+                  <tr className="simpletable__title">
+                    <th className="simpletable__title__field--playerdetail">
+                      Receive
+                    </th>
+                    <th className="simpletable__title__field--playerdetail"></th>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Positive</td>
+                    <td className="simpletable__row__field">
+                      {player.receive.positive}
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Negative</td>
+                    <td className="simpletable__row__field">
+                      {player.receive.negative}
+                    </td>
+                  </tr>
+                  <tr className="simpletable__row">
+                    <td className="simpletable__row__field">Errors</td>
+                    <td className="simpletable__row__field">
+                      {player.receive.error}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </>
+        ) : null}
 
         <button
           onClick={() => {
